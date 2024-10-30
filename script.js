07b281ad9ca1f52039082cf92f1878143e90e73b
Steve <173348548+s0lumen@users.noreply.github.com>
2024-10-30 23:57:14 +0800

    starting the project

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+function getComputerChoice(ComputerChoice) {
+    let rps = Math.floor(Math.random() * 3) + 1;
+
+}
+
+getComputerChoice();
+
+function getHumanChoice(HumanChoice) {
+    return HumanChoice;
+}
+
+function playRound(HC, CC) {
+
+}
